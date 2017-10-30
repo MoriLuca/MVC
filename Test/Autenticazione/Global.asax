f8a1dc477876b454f8db45ece1135b72cd47104f
MoriLuca <mori.luca@hotmail.it>
2017-10-30 13:58:51 +0100

    Creazione Progetto

---
+++ Test/Autenticazione/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Autenticazione.MvcApplication" Language="C#" %>
---
+++ Test/Autenticazione/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Autenticazione.Startup))]
+namespace Autenticazione
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
